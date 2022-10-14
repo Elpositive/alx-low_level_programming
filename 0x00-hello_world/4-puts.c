1f3e87675166c62f7283b61682a2e4311e9be83d
Elpositive <davidolubowale@gmail.com>
2022-10-13 22:40:41 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-/*
- *  *main- Printing
+/**
+ *  *main- Printing in C
  *   *
  *  *Return: Always zero
  */
+
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
+
 }
