f3f19d99faf520248425ba6f8bb4cce7627f6ae9
Elpositive <davidolubowale@gmail.com>
2022-10-17 21:53:06 -0700

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print all combinations of single digits.
+ * main - prints all possible combinations of single-digit numbers
  *
- * Return:0 means success, non-zero if otherwise.
+ * Return: Always 0
  */
+
 int main(void)
 {
 	int num;
@@ -19,9 +20,11 @@ int main(void)
 		else
 		{
 			putchar(',');
+
 			putchar(' ');
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
